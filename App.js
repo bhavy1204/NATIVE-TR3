2c0bee92b63a73f7b07d1842926207b53d8cb2f6
ritesh <riteshpatidar088@gmail.com>
2025-10-28 15:39:27 +0530

    upd : button component

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import newimg from './assets/favicon.png'
+function App() {
 
-export default function App() {
+  const handleButtonPress = () => {
+    console.log('pressed') 
+    Alert.alert('button')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+        // style={{
+        //   flex: 1,
+        //   flexDirection: 'row',
+        //   justifyContent: 'center',
+        //   alignItems: 'center',
+        // }}
+        >
+          <View style={styles.box1}>
+            <Text>Box1</Text>
+          </View>
+          <View style={{ backgroundColor: 'blue' }}>
+            <Text>box 2</Text>
+          </View>
+        </View>
+        <Image
+          style={styles.img}
+          source={{
+            uri: 'https://th.bing.com/th/id/OIP.UvIl8jMyLHSF79JRYKTJnwHaEU?w=286&h=180&c=7&r=0&o=7&dpr=1.4&pid=1.7&rm=3',
+          }}
+        />
+        <Image source={newimg} style={styles.img} />
+        <Button title='CLICK' onPress={handleButtonPress} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  box1: {
+    backgroundColor: 'orange',
+    width: 100,
+    height: 100,
+  },
+  img: {
+    height: 100,
+    width: 100,
   },
 });
